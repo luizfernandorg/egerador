1c29a49686cda24b2eb090108ff636187e4f8e5d
Luiz Fernando <luizfernandorg@gmail.com>
2022-01-03 07:32:55 -0300

    Removed the Security Key
    Removed the Security Key

--- egerador/settings.py
+++ egerador/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'eq+fw796(bf!s03rhbl)3a^ct!jfl7^rio7cja4s0qvbt+-l_%'
+SECRET_KEY = 'create your key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
